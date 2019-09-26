293503d49af0834eab96a2b0f187cc136af51cf9
vendetta78 <55823091+vendetta78@users.noreply.github.com>
2019-09-26 12:01:09 +0700

    Add files via upload

---
+++ PBO1-10118031-Latihan7-Integer/src/pbo1/pkg10118031/latihan7/integer/PBO110118031Latihan7Integer.java
@@ -0,0 +1,33 @@
+package pbo1.pkg10118031.latihan7.integer;
+
+/**
+ *
+ * @author Vendetta 
+ * NAMA              : Rizki Aria Putra 
+ * KELAS             : PBO1 
+ * NIM               : 10118031
+ * Deskripsi Program : Program ini berisi program untuk 
+                       varibel tipe data Integer
+ */
+public class PBO110118031Latihan7Integer {
+
+    public static void main(String[] args) {
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("byte : "+ b);
+        System.out.println("short : "
+                + s);
+        System.out.println("int : "
+                + i);
+        System.out.println("long : "
+                + l);
+
+    }
+
+}
